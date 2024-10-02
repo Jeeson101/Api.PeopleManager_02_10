640a54177f816d08d62877934717636981a0c777
Jeeson101 <129368352+Jeeson101@users.noreply.github.com>
2024-10-02 17:32:24 +0200

    test

--- PeopleManager.Sdk/Extensions/ServiceCollectionExtensions.cs
+++ PeopleManager.Sdk/Extensions/ServiceCollectionExtensions.cs
@@ -11,13 +11,11 @@ namespace PeopleManager.Sdk.Extensions
 	{
 		public static IServiceCollection AddApi(this IServiceCollection services, string apiUrl)
 		{
-			services.AddHttpClient("PeopleManagerApi", options =>
-			{
-				options.BaseAddress = new Uri(apiUrl);
-			});
+			services.AddHttpClient("PeopleManagerApi", options => { options.BaseAddress = new Uri(apiUrl); });
 
 			services.AddScoped<OrganizationSdk>();
 
 			return services;
 		}
+	}
 }
